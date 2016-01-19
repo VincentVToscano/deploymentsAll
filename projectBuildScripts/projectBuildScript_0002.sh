7b5169886f59e329041b8f5f6fe77e0c6cda7caf
Vincent V. Toscano <vincent@toolofna.com>
2016-01-19 11:38:08 -0800

    projectBuildScript_0002 updated
    projectBuildScript_0002 updated

--- projectBuildScripts/projectBuildScript_0002.sh
+++ projectBuildScripts/projectBuildScript_0002.sh
@@ -6,29 +6,26 @@
 # git clone https://github.com/VincentVToscano/deploymentsAll.git ~/clone/deploymentsAll
 
 # Run build
-# . ~/clone/deploymentsAll/projectBuildScripts/projectBuildScript_0001.sh
+# . ~/clone/deploymentsAll/projectBuildScripts/projectBuildScript_0002.sh
 YEAR=`date +'%Y'`
 
 # Create build and Git deploy directories
-mkdir -p ~/clone/HTML_5_build && mkdir -p ~/clone/XHTML_1_Transitional_build
+mkdir -p ~/clone/www
 
-# Copy template all build directories
-echo $HOME/clone/HTML_5_build/build.html $HOME/clone/XHTML_1_Transitional_build/build.html | xargs -n 1 cp $HOME/clone/deploymentsAll/buildTemplates/build.tpl
-
-# Point to src environments
-HTML_5_SRC=~/clone/HTML_5_src
-XHTML_1_SRC=~/clone/XHTML_1_Transitional_src
+# Copy template to all build directories
+echo $HOME/clone/www/build.html | xargs -n 1 cp $HOME/clone/deploymentsAll/buildTemplates/build.tpl
 
 # Point to src environments
-HTML_5_BUILD=~/clone/HTML_5_build
-XHTML_1_BUILD=~/clone/XHTML_1_Transitional_build
+HTML_5_SRC=~/clone/src
+
+# Point to src environments
+HTML_5_BUILD=~/clone/www
 
 # Create reference to build.html
-BUILD_PAGE=~/clone/HTML_5_build/build.html
-BUILD_PAGE2=~/clone/XHTML_1_Transitional_build/build.html
+BUILD_PAGE=~/clone/www/build.html
 
 # Update year
-echo $BUILD_PAGE $BUILD_PAGE2 | xargs -n 1 sed -i -e "s:CURRENT_YEAR:$YEAR:"
+echo $BUILD_PAGE | xargs -n 1 sed -i -e "s:CURRENT_YEAR:$YEAR:"
 
 # Source NVM
 . ~/.nvm/nvm.sh
@@ -45,7 +42,7 @@ case "$CI_BRANCH" in
         cd $HTML_5_SRC
         npm cache clean && npm install
         echo "<div>&#x2592;&#x2592;&#x2592;&nbsp;&nbsp;&nbsp;&nbsp;HTML_5_build&nbsp;&nbsp;&nbsp;&nbsp;&#x2592;&#x2592;&#x2592;</div><h1>Gulp Build</h1><pre>" >> ${BUILD_PAGE}
-        sh buildPages.sh 2>&1 >> ${BUILD_PAGE}
+        gulp 2>&1 >> ${BUILD_PAGE}
         echo '</pre>' >> ${BUILD_PAGE}
         echo '<h1>Files and Directories</h1><pre class="sm">' >> ${BUILD_PAGE}
         (find * 2>&1) >> ${BUILD_PAGE}
@@ -60,33 +57,49 @@ case "$CI_BRANCH" in
         echo $CI_MESSAGE
         sed -i "s:CI_MESSAGE:$CI_MESSAGE:" ${BUILD_PAGE} || sed -i "s:CI_MESSAGE:Merge:" ${BUILD_PAGE}
         sed -i "s:LLL: :g" ${BUILD_PAGE}
+    ;;
 
-        # Build XHTML_1_Transitional_build
-        cd $XHTML_1_SRC
+    stage)
+
+        cd $HTML_5_SRC
         npm cache clean && npm install
-        echo "<div>&#x2592;&#x2592;&#x2592;&nbsp;&nbsp;&nbsp;&nbsp;XHTML_1_Transitional_build&nbsp;&nbsp;&nbsp;&nbsp;&#x2592;&#x2592;&#x2592;</div><h1>Gulp Build</h1><pre>" >> ${BUILD_PAGE2}
-        sh buildPages.sh 2>&1 >> ${BUILD_PAGE2}
-        echo '</pre>' >> ${BUILD_PAGE2}
-        echo '<h1>Src Files and Directories</h1><pre class="sm">' >> ${BUILD_PAGE2}
-        (find * 2>&1) >> ${BUILD_PAGE2}
-        echo '</pre>' >> ${BUILD_PAGE2}
-        cd $XHTML_1_BUILD
-        echo '<h1>Build and Directories</h1><pre class="sm">' >> ${BUILD_PAGE2}
-        (find * 2>&1) >> ${BUILD_PAGE2}
-        echo '</pre></body></html>' >> $BUILD_PAGE2
-        sed -i -e "s:CI_BUILD_NUMBER:$CI_BUILD_NUMBER:" -e "s:CI_COMMIT_ID:$CI_COMMIT_ID:" -e "s:CI_BRANCH:$CI_BRANCH:" -e "s:CI_COMMITTER_NAME:$CI_COMMITTER_NAME:" ${BUILD_PAGE2}
+        echo "<div>&#x2592;&#x2592;&#x2592;&nbsp;&nbsp;&nbsp;&nbsp;HTML_5_build&nbsp;&nbsp;&nbsp;&nbsp;&#x2592;&#x2592;&#x2592;</div><h1>Gulp Build</h1><pre>" >> ${BUILD_PAGE}
+        gulp --stage >> ${BUILD_PAGE}
+        echo '</pre>' >> ${BUILD_PAGE}
+        echo '<h1>Files and Directories</h1><pre class="sm">' >> ${BUILD_PAGE}
+        (find * 2>&1) >> ${BUILD_PAGE}
+        echo '</pre>' >> ${BUILD_PAGE}
+        cd $HTML_5_BUILD
+        echo '<h1>Build and Directories</h1><pre class="sm">' >> ${BUILD_PAGE}
+        (find * 2>&1) >> ${BUILD_PAGE}
+        echo '</pre></body></html>' >> $BUILD_PAGE
+        sed -i -e "s:CI_BUILD_NUMBER:$CI_BUILD_NUMBER:" -e "s:CI_COMMIT_ID:$CI_COMMIT_ID:" -e "s:CI_BRANCH:$CI_BRANCH:" -e "s:CI_COMMITTER_NAME:$CI_COMMITTER_NAME:" ${BUILD_PAGE}
         echo $CI_MESSAGE
         CI_MESSAGE=$(echo $CI_MESSAGE | sed 's/ /LLL/g')
         echo $CI_MESSAGE
-        sed -i "s:CI_MESSAGE:$CI_MESSAGE:" ${BUILD_PAGE2} || sed -i "s:CI_MESSAGE:Merge:" ${BUILD_PAGE2}
-        sed -i "s:LLL: :g" ${BUILD_PAGE2}
+        sed -i "s:CI_MESSAGE:$CI_MESSAGE:" ${BUILD_PAGE} || sed -i "s:CI_MESSAGE:Merge:" ${BUILD_PAGE}
+        sed -i "s:LLL: :g" ${BUILD_PAGE}
+    ;;
+    master)
 
-        # Change forwarding in .htaccess
-        if [ "$CI_BRANCH" = 'stage' ]; then
-            sed -i "s:dev.t:stage.t:g" "$HTML_5_BUILD"/.htaccess
-            sed -i "s:dev.t:stage.t:g" "$XHTML_1_BUILD"/.htaccess
-        fi
-
+        cd $HTML_5_SRC
+        npm cache clean && npm install
+        echo "<div>&#x2592;&#x2592;&#x2592;&nbsp;&nbsp;&nbsp;&nbsp;HTML_5_build&nbsp;&nbsp;&nbsp;&nbsp;&#x2592;&#x2592;&#x2592;</div><h1>Gulp Build</h1><pre>" >> ${BUILD_PAGE}
+        gulp --production >> ${BUILD_PAGE}
+        echo '</pre>' >> ${BUILD_PAGE}
+        echo '<h1>Files and Directories</h1><pre class="sm">' >> ${BUILD_PAGE}
+        (find * 2>&1) >> ${BUILD_PAGE}
+        echo '</pre>' >> ${BUILD_PAGE}
+        cd $HTML_5_BUILD
+        echo '<h1>Build and Directories</h1><pre class="sm">' >> ${BUILD_PAGE}
+        (find * 2>&1) >> ${BUILD_PAGE}
+        echo '</pre></body></html>' >> $BUILD_PAGE
+        sed -i -e "s:CI_BUILD_NUMBER:$CI_BUILD_NUMBER:" -e "s:CI_COMMIT_ID:$CI_COMMIT_ID:" -e "s:CI_BRANCH:$CI_BRANCH:" -e "s:CI_COMMITTER_NAME:$CI_COMMITTER_NAME:" ${BUILD_PAGE}
+        echo $CI_MESSAGE
+        CI_MESSAGE=$(echo $CI_MESSAGE | sed 's/ /LLL/g')
+        echo $CI_MESSAGE
+        sed -i "s:CI_MESSAGE:$CI_MESSAGE:" ${BUILD_PAGE} || sed -i "s:CI_MESSAGE:Merge:" ${BUILD_PAGE}
+        sed -i "s:LLL: :g" ${BUILD_PAGE}
     ;;
     special)
         echo -n "fallthrough"
